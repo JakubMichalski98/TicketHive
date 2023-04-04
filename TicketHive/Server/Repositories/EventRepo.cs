da219350b1f08749fdd8ad796dfa2775625cc39a
serein3 <jakubmichalski9818@gmail.com>
2023-04-04 13:04:41 +0200

    Add EventRepo

---
+++ TicketHive/Server/Repositories/EventRepo.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketHive.Server.Data;
+using TicketHive.Shared.Models;
+
+namespace TicketHive.Server.Repositories
+{
+    public class EventRepo : IEventRepo
+    {
+        private readonly EventDbContext context;
+
+        public EventRepo(EventDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<EventModel>> GetAllEvents()
+        {
+            return await context.Events.Include(e => e.Users).ToListAsync();
+        }
+
+        public async Task<EventModel?> GetEvent(int id)
+        {
+            EventModel? eventModel = await context.Events.Include(e => e.Users).SingleOrDefaultAsync(e => e.Id == id);
+
+            return eventModel;
+        }
+        public async Task AddEvent(EventModel eventToAdd)
+        {
+            context.Events.Add(eventToAdd);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task RemoveEvent(int id)
+        {
+            var eventToRemove = await context.Events.FindAsync(id);
+
+            if (eventToRemove != null)
+            {
+                context.Events.Remove(eventToRemove);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ TicketHive/Server/Repositories/IEventRepo.cs
@@ -0,0 +1,12 @@
+﻿using TicketHive.Shared.Models;
+
+namespace TicketHive.Server.Repositories
+{
+    public interface IEventRepo
+    {
+        public Task<List<EventModel>> GetAllEvents();
+        public Task<EventModel?> GetEvent(int id);
+        public Task AddEvent(EventModel eventToAdd);
+        public Task RemoveEvent(int id);
+    }
+}
