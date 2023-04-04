ac7abd925be32c7015601f8e418ca606562b5275
Alexanderrosen <alexander.rosen99@outlook.com>
2023-04-04 11:29:27 +0200

    Added Login-Register-ViewImports

---
+++ TicketHive/Server/Areas/Identity/Pages/Account/LogIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TicketHive.Server.Areas.Identity.Pages.Account
+{
+    public class LogInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
