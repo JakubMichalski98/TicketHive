f822651432fe0775354257e6ff2241dad37ede45
serein3 <jakubmichalski9818@gmail.com>
2023-04-04 15:29:27 +0200

    Add EventsController with GET endpoints

---
+++ TicketHive/Client/Repositories/EventRepo.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Json;
+using TicketHive.Shared.Models;
+
+namespace TicketHive.Client.Repositories
+{
+    public class EventRepo : IEventRepo
+    {
+        private readonly HttpClient httpClient;
+
+        public List<EventModel> Events { get; set; } = new();
+
+        public EventRepo(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task GetAllEvents()
+        {
+            var result = await httpClient.GetFromJsonAsync<List<EventModel>>("api/events");
+
+            if (result != null)
+            {
+                Events = result;
+            }
+        }
+
+        public Task<EventModel?> GetEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task AddEvent(EventModel eventToAdd)
+        {
+            throw new NotImplementedException();
+        }
+        public Task RemoveEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
