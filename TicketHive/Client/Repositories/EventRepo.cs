3ec53ef15af55e961962c17de3d86232f1e0bb50
Jakub MIchalski <66911458+serein3@users.noreply.github.com>
2023-04-05 14:53:10 +0200

    Add crud methods to Server and Client

--- TicketHive/Client/Repositories/EventRepo.cs
+++ TicketHive/Client/Repositories/EventRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Net.Http.Json;
+using System.Text.Json;
 using TicketHive.Shared.Models;
 
 namespace TicketHive.Client.Repositories
@@ -27,15 +28,37 @@ namespace TicketHive.Client.Repositories
 
         public async Task<EventModel?> GetEvent(int id)
         {
-            throw new NotImplementedException();
+            var response = await httpClient.GetAsync($"api/events/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var foundEvent = await response.Content.ReadFromJsonAsync<EventModel>();
+                return foundEvent;
+            }
+            return null;
         }
-        public Task AddEvent(EventModel eventToAdd)
+        public async Task AddEvent(EventModel eventToAdd)
         {
-            throw new NotImplementedException();
+            var result = await httpClient.PostAsJsonAsync("api/events", eventToAdd);
+
+            await SetEvents(result);
         }
-        public Task RemoveEvent(int id)
+
+        public async Task RemoveEvent(int id)
         {
-            throw new NotImplementedException();
+            var result = await httpClient.DeleteAsync($"api/events{id}");
+            await SetEvents(result);
+        }
+        
+        public async Task UpdateEvent(EventModel updatedEvent)
+        {
+            var result = await httpClient.PutAsJsonAsync("api/events", updatedEvent);
+            await SetEvents(result);
+        }
+        private async Task SetEvents(HttpResponseMessage? result)
+        {
+            var response = await result.Content.ReadFromJsonAsync<List<EventModel>>();
+            Events = response;
         }
     }
 }
